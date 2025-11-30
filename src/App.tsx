84889b14beb35baeefada2bf6afaf6d186da512c
Mateo Urrutia <retrogaming251@gmail.com>
2025-11-30 20:38:42 -0300

    Initial commit: Quiver Desktop application

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Header1 } from "./components/quiver/typography";
+import { Button } from "./components/ui/button";
+
+function App() {
+	return (
+		<main class="bg-transparent">
+			<Header1>Hello World</Header1>
+			<Button>
+				Click me
+			</Button>
+		</main>
+	);
+}
+
+export default App;
