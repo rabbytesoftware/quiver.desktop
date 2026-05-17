2ef3bf5a64ebc1d9174f6cdf8f9769d9b7588e76
Mateo Urrutia <me@char2cs.net>
2026-05-17 19:31:03 -0300

    feat: add ResizeHandle for sidebar drag-to-resize

---
+++ src/components/ResizeHandle.tsx
@@ -0,0 +1,33 @@
+import { useRef } from 'react'
+import { useUIStore } from '@/store/ui'
+
+export function ResizeHandle() {
+  const setSidebarWidth = useUIStore((s) => s.setSidebarWidth)
+  const sidebarWidth = useUIStore((s) => s.sidebarWidth)
+  const startX = useRef(0)
+  const startWidth = useRef(0)
+
+  const onMouseDown = (e: React.MouseEvent) => {
+    startX.current = e.clientX
+    startWidth.current = sidebarWidth
+
+    const onMouseMove = (e: MouseEvent) => {
+      setSidebarWidth(startWidth.current + (e.clientX - startX.current))
+    }
+
+    const onMouseUp = () => {
+      window.removeEventListener('mousemove', onMouseMove)
+      window.removeEventListener('mouseup', onMouseUp)
+    }
+
+    window.addEventListener('mousemove', onMouseMove)
+    window.addEventListener('mouseup', onMouseUp)
+  }
+
+  return (
+    <div
+      className="w-[3px] flex-shrink-0 cursor-col-resize bg-white/[0.05] transition-colors hover:bg-white/[0.18]"
+      onMouseDown={onMouseDown}
+    />
+  )
+}
